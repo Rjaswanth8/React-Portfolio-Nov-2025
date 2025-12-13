2b961dcce19599d228f998656f6a50b3bac02d6c
Jaswanth Chowdary <kittunaidu888@gmail.com>
2025-12-13 21:08:20 +0530

    new change

--- server/Server.js
+++ server/Server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+
 require("dotenv").config();
 const path = require("path");
 
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
    MIDDLEWARE
 =============================== */
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 /* ===============================
@@ -64,7 +64,9 @@ app.post("/api/contact", async (req, res) => {
     const newContact = new Contact({ name, phone, email, subject, message });
     await newContact.save();
 
-    res.status(201).json({ success: true, message: "Message sent successfully!" });
+    res
+      .status(201)
+      .json({ success: true, message: "Message sent successfully!" });
   } catch (err) {
     console.error("❌ Error saving contact:", err);
     res.status(500).json({
